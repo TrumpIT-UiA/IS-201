08947ef9e0d27998965c34a3e45ea18aceafd312
sannitie <snorre_s92@hotmail.com>
2017-09-28 11:01:34 +0200

    1

---
+++ Telenorliga.eksempel.sql
@@ -0,0 +1,60 @@
+use Testnicolay;
+
+/*
+create table lag (
+lagnavn varchar(100),
+Spilt int,
+Vunnet int,
+Uavgjort int,
+Tapt int,
+Differanse int,
+Straff int,
+Poeng int,
+constraint lagnavn_pk primary key (lagnavn)
+);
+
+create table spiller (
+steamid varchar(20),
+navn varchar(50),
+nasjonalitet varchar(50),
+constraint steamid_pk primary key (steamid)
+);
+
+insert into lag (lagnavn, spilt, vunnet, uavgjort, tapt, differanse, straff, poeng)
+values ("Refu5ed", 4, 2, 2, 0, 4, 0, 9);
+
+insert into lag (lagnavn, spilt, vunnet, uavgjort, tapt, differanse, straff, poeng)
+values ("ChokeMasters", 4, 4, 0, 0, 8, 0, 12);
+
+insert into lag (lagnavn, spilt, vunnet, uavgjort, tapt, differanse, straff, poeng)
+values ("D!E", 4, 2, 1, 1, 2, 0, 7);
+
+insert into lag (lagnavn, spilt, vunnet, uavgjort, tapt, differanse, straff, poeng)
+values ("A-laje", 5, 2, 1, 2, 0, 0, 7);
+
+insert into lag (lagnavn, spilt, vunnet, uavgjort, tapt, differanse, straff, poeng)
+values ("KEYFOX", 4, 1, 2, 1, 0, 0, 5);
+
+insert into lag (lagnavn, spilt, vunnet, uavgjort, tapt, differanse, straff, poeng)
+values ("eKSEPSJONELT", 5, 1 , 2, 2, -2, 0, 5);
+
+insert into lag (lagnavn, spilt, vunnet, uavgjort, tapt, differanse, straff, poeng)
+values ("ESEL eSports", 5, 1, 0, 4, -6, 0, 3);
+
+insert into lag (lagnavn, spilt, vunnet, uavgjort, tapt, differanse, straff, poeng)
+values ("Riddle Elektroniske Sportsklubb CS:GO", 5, 0, 2, 3, -6, 0, 2);
+*/
+
+/*
+update lag
+set spilt = 4
+where lagnavn = "Refu5ed";
+
+select *
+from lag
+order by `Poeng` desc;
+
+select *
+from spiller
+*/
+
