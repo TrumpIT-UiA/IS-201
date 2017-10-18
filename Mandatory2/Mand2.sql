ad66f07e78210d15ed554dd6953ab2e0e4475f72
sannitie <snorre_s92@hotmail.com>
2017-10-18 11:37:34 +0200

    fikset foreign keys

--- Mandatory2/Mand2.sql
+++ Mandatory2/Mand2.sql
@@ -8,24 +8,9 @@ Stock varchar(100) not null,
 RecorderLevel varchar(100)
 );
 
-create table if not exists `Order`(
-OrderID int not null auto_increment primary key,
-CustomerID int not null,
-foreign key (CustomerID) references Customer(CustomerID),
-Order_date varchar(100) not null,
-Billing_address varchar(100) not null,
-Estimated_delivery_date varchar(100),
-Order_status varchar(100),
-InvoiceID int not null,
-foreign key (InvoiceID) references Invoice(InvoiceID)
-);
-
-create table if not exists OrderLine(
-OrderID int not null,
-foreign key (OrderID) references `Order`(OrderID),
-ProductID int not null,
-foreign key (ProductID) references Product(ProductID),
-Quantity int not null
+create table if not exists Corporation(
+CorporationID int not null auto_increment primary key,
+CorporationName varchar(100) not null
 );
 
 create table if not exists Customer(
@@ -41,13 +26,6 @@ CorporationID int,
 foreign key (CorporationID) references Corporation(CorporationID)
 );
 
-create table if not exists Corporation(
-CorporationID int not null auto_increment primary key,
-CorporationName varchar(100) not null
-);
-
-/* Mulig de neste tabellene må endres etter tilbakemelding fra divinder */
-
 create table if not exists Invoice(
 InvoiceID int not null auto_increment primary key,
 DateIssued varchar(100),
@@ -55,6 +33,25 @@ Paid boolean not null,
 Amount int not null
 );
 
+create table if not exists `Order`(
+OrderID int not null auto_increment primary key,
+CustomerID int not null,
+Order_date varchar(100) not null,
+Billing_address varchar(100) not null,
+Estimated_delivery_date varchar(100),
+Order_status varchar(100),
+InvoiceID int not null,
+constraint foreign key (CustomerID) references Customer(CustomerID),
+constraint foreign key (InvoiceID) references Invoice(InvoiceID)
+);
+
+create table if not exists OrderLine(
+OrderID int not null,
+foreign key (OrderID) references `Order`(OrderID),
+ProductID int not null,
+foreign key (ProductID) references Product(ProductID),
+Quantity int not null
+);
 
 create table if not exists CreditCard(
 CardNr varchar(100) not null,
@@ -65,8 +62,4 @@ ExpirationDate varchar(100) not null,
 CardHolderName varchar(100) not null,
 CVC varchar(3) not null
 );
-
-
-
-/* Slutt på tabeller som muligens må endres */
 
