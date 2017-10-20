b9b407b55c08d4654e8d892277fa0f3cef76877c
sannitie <snorre_s92@hotmail.com>
2017-10-20 16:37:09 +0200

    Ferdig me mand2

--- Mandatory2/Mand2.sql
+++ Mandatory2/Mand2.sql
@@ -1,12 +1,12 @@
 use mand2;
 
-/*
+
 create table if not exists Product(
 ProductID int not null auto_increment primary key,
 Name varchar(100) not null,
 Price int not null,
 Stock int not null,
-RecorderLevel varchar(100)
+ReorderLevel varchar(100)
 );
 
 create table if not exists Corporation(
@@ -64,76 +64,175 @@ CardHolderName varchar(100) not null,
 CVC varchar(3) not null
 );
 
-insert into Product (ProductID, Name, Price, Stock)
-values (1, "a", 1, 1);
+# Alle IDene, som er primary keys blir auto_increment , som betyr at de ikke trenger
+# å bli gitt en verdi fordi det skjer automatisk.
 
-insert into Product (ProductID, Name, Price, Stock)
-values (2, "b", 2, 2);
+insert into Product (Name, Price, Stock, ReorderLevel)
+values ("Chair", 299, 8, 6);
 
-insert into Product (ProductID, Name, Price, Stock)
-values (3, "c", 3, 3);
+insert into Product (Name, Price, Stock, ReorderLevel)
+values ("Table", 999, 3, 2);
 
-insert into Corporation (CorporationID, CorporationName)
-values (1, "a");
+insert into Product (Name, Price, Stock, ReorderLevel)
+values ("Door", 1249, 2, 3);
 
-insert into Corporation (CorporationID, CorporationName)
-values (2, "b");
+insert into Product (Name, Price, Stock, ReorderLevel)
+values ("TV", 4999, 5, 2);
 
-insert into Corporation (CorporationID, CorporationName)
-values (3, "c");
+insert into Product (Name, Price, Stock, ReorderLevel)
+values ("Window", 2299, 3, 6);
 
-insert into Customer (CustomerID, First_name, Last_name, Address, Zipcode, Email_address, Country, CorporationID)
-values (1, "a", "z", "vei1", "1", "1@uia.no", "norge", 1);
+insert into Product (Name, Price, Stock, ReorderLevel)
+values ("Seng", 2999, 2, 1);
 
-insert into Customer (CustomerID, First_name, Last_name, Address, Zipcode, Email_address, Country, CorporationID)
-values (2, "b", "y", "vei2", "2", "2@uia.no", "sverige", 2);
+insert into Corporation (CorporationName)
+values ("Kjell & co");
 
-insert into Customer (CustomerID, First_name, Last_name, Address, Zipcode, Email_address, Country, CorporationID)
-values (3, "c", "x", "vei3", "3", "3@uia.no", "norge", 3);
+insert into Corporation (CorporationName)
+values ("UiA");
 
-insert into Invoice (InvoiceID, DateIssued, Paid, Amount)
-values (1, "1.1", true, 1);
+insert into Customer (First_name, Last_name, Address, Zipcode, Email_address, Country, CorporationID)
+values ("Emil", "Ruud", "Kvernveien 7b", "4630", "Emilr15@uia.no", "Norge", 2);
 
-insert into Invoice (InvoiceID, DateIssued, Paid, Amount)
-values (2, "1.2", true, 2);
+insert into Customer (First_name, Last_name, Address, Zipcode, Email_address, Country, CorporationID)
+values ("Snorre", "Tunge", "Skogveien 14a", "4630", "Snorrt13@uia.no", "Norge", 2);
 
-insert into Invoice (InvoiceID, DateIssued, Paid, Amount)
-values (3, "1.3", true, 3);
+insert into Customer (First_name, Last_name, Address, Zipcode, Email_address, Country, CorporationID)
+values ("Marius", "Norli", "Skogveien 7b", "4630", "mariug16@uia.no", "Norge", 1);
 
-insert into `Order` (OrderID, CustomerID, Order_date, Billing_address, Estimated_delivery_date, InvoiceID)
-values (1, 1, "1.1", "vei1", "2.1", 1);
+insert into Customer (First_name, Last_name, Address, Zipcode, Email_address, Country, CorporationID)
+values ("Nicolay", "Leknes", "Skogveien 7b", "4630", "Nicolal@uia.no", "Norge", 2);
 
-insert into `Order` (OrderID, CustomerID, Order_date, Billing_address, Estimated_delivery_date, InvoiceID)
-values (2, 2, "1.2", "vei2", "2.2", 2);
+insert into Customer (First_name, Last_name, Address, Zipcode, Email_address, Country, CorporationID)
+values ("Vebjørn", "Nordby", "Skogveien 7b", "4630", "Vebjn16@uia.no", "Norge", 1);
 
-insert into `Order` (OrderID, CustomerID, Order_date, Billing_address, Estimated_delivery_date, InvoiceID)
-values (3, 3, "1.1", "vei1", "2.1", 1);
+insert into Customer (First_name, Last_name, Address, Zipcode, Email_address, Country, CorporationID)
+values ("Nikolai", "Dyb", "Dronningens gate 52", "4608", "Nikold@uia.no", "Norge", 2);
+
+insert into Invoice (DateIssued, Paid, Amount)
+values ("09.09.17", true, 2999); #1 seng
+
+insert into Invoice (DateIssued, Paid, Amount)
+values ("11.09.17", true, 5998); #2 senger
+
+insert into Invoice (DateIssued, Paid, Amount)
+values ("12.09.17", true, 9998); #2 TV
+
+insert into Invoice (DateIssued, Paid, Amount)
+values ("17.09.17", true, 2299); #1 vindu
+
+insert into Invoice (DateIssued, Paid, Amount)
+values ("14.10.17", true, 1794); #6 stoler
+
+insert into Invoice (DateIssued, Paid, Amount)
+values ("17.10.17", false, 6897); #3 vinduer
+
+insert into Invoice (DateIssued, Paid, Amount)
+values ("17.10.17", true, 2498); #2 dører
+
+insert into Invoice (DateIssued, Paid, Amount)
+values ("19.10.17", false, 4999); #1 TV
+
+insert into Invoice (DateIssued, Paid, Amount)
+values ("20.10.17", false, 1896); #1 table - 4 chairs
+
+insert into Invoice (DateIssued, Paid, Amount)
+values ("20.10.17", false, 7845); #1 TV - 1 table - 2 chairs - 1 door
+
+insert into `Order` (CustomerID, Order_date, Billing_address, Estimated_delivery_date, InvoiceID)
+values (2, "09.09.17", "Skogveien 14a", "13.09.17", 1);
+
+insert into `Order` (CustomerID, Order_date, Billing_address, Estimated_delivery_date, InvoiceID)
+values (5, "11.09.17", "Skogveien 7b", "15.09.17", 2);
+
+insert into `Order` (CustomerID, Order_date, Billing_address, Estimated_delivery_date, InvoiceID)
+values (5, "12.09.17", "Skogveien 7b", "16.09.17", 3);
+
+insert into `Order` (CustomerID, Order_date, Billing_address, Estimated_delivery_date, InvoiceID)
+values (2, "17.09.17", "Skogvenei 14a", "21.09.17", 4);
+
+insert into `Order` (CustomerID, Order_date, Billing_address, Estimated_delivery_date, InvoiceID)
+values (3, "14.10.17", "Skogveien 7b", "18.10.17", 5);
+
+insert into `Order` (CustomerID, Order_date, Billing_address, Estimated_delivery_date, InvoiceID)
+values (2, "17.10.17", "Skogveien 14a", "21.10.17", 6);
+
+insert into `Order` (CustomerID, Order_date, Billing_address, Estimated_delivery_date, InvoiceID)
+values (5, "17.10.17", "Skogveien 7b", "21.10.17", 7);
+
+insert into `Order` (CustomerID, Order_date, Billing_address, Estimated_delivery_date, InvoiceID)
+values (6, "19.10.17", "Dronningens gate 52", "23.10.17", 8);
+
+insert into `Order` (CustomerID, Order_date, Billing_address, Estimated_delivery_date, InvoiceID)
+values (1, "20.10.17", "Kverneveien 7b", "24.10.17", 9);
+
+insert into `Order` (CustomerID, Order_date, Billing_address, Estimated_delivery_date, InvoiceID)
+values (4, "20.10.17", "Skogveien 7b", "24.10.17", 10);
 
 insert into OrderLine (OrderID, ProductID, Quantity)
-values (1, 1, 1);
+values (1, 6, 1);
 
 insert into OrderLine (OrderID, ProductID, Quantity)
-values (2, 2, 2);
+values (2, 6, 2);
 
 insert into OrderLine (OrderID, ProductID, Quantity)
-values (3, 3, 3);
+values (3, 4, 2);
+
+insert into OrderLine (OrderID, ProductID, Quantity)
+values (4, 5, 1);
+
+insert into OrderLine (OrderID, ProductID, Quantity)
+values (5, 1, 6);
+
+insert into OrderLine (OrderID, ProductID, Quantity)
+values (6, 5, 3);
+
+insert into OrderLine (OrderID, ProductID, Quantity)
+values (7, 3, 2);
+
+insert into OrderLine (OrderID, ProductID, Quantity)
+values (8, 4, 1);
+
+insert into OrderLine (OrderID, ProductID, Quantity)
+values (9, 1, 4);
+
+insert into OrderLine (OrderID, ProductID, Quantity)
+values (9, 3, 2);
+
+insert into OrderLine (OrderID, ProductID, Quantity)
+values (10, 4, 1);
+
+insert into OrderLine (OrderID, ProductID, Quantity)
+values (10, 2, 1);
+
+insert into OrderLine (OrderID, ProductID, Quantity)
+values (10, 1, 2);
+
+insert into OrderLine (OrderID, ProductID, Quantity)
+values (10, 3, 1);
 
 insert into CreditCard (CardNr, InvoiceID, ExpirationDate, CardHolderName, CVC)
-values ("1234567891234567", 1, "1.1", "a", "123");
+values ("4925570008629865", 1, "08/19", "Snorre Tunge", "901");
 
 insert into CreditCard (CardNr, InvoiceID, ExpirationDate, CardHolderName, CVC)
-values ("1234567891234568", 2, "1.2", "b", "234");
+values ("4369957236080049", 3, "12/20", "Nikolai Dyb", "322");
 
-insert into CreditCard (CardNr, InvoiceID, ExpirationDate, CardHolderName, CVC)
-values ("1234567891234569", 3, "1.3", "b", "345");
 
-*/
-
-select Customer.First_name, Customer.Last_name, OrderLine.Quantity, Product.`Name`
-from Customer
+# Oppgave 4a
+select Customer.First_name, Customer.Last_name, OrderLine.Quantity, Product.`Name`from Customer
 inner join `Order`
 on Customer.CustomerID = `Order`.OrderID
 inner join OrderLine
 on `Order`.OrderID = OrderLine.OrderID
 inner join Product
 on OrderLine.ProductID = Product.ProductID;
+
+
+# Oppgave 4b
+select Product.`Name`, sum(Quantity) as TotalQuantity, sum(Quantity)*Product.Price as TotalSum
+from OrderLine, Product
+where OrderLine.ProductID = Product.ProductID
+group by Product.ProductID asc
+order by TotalSum desc
+limit 3;
+
